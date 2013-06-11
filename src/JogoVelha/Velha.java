1a03482287f22fb3f82f0f339fdbb85e72e8bc3c
Thayso <thaysohenrique@gmail.com>
2013-06-11 20:21:29 -0300

    Create Velha.java

---
+++ src/JogoVelha/Velha.java
@@ -0,0 +1,67 @@
+package JogoVelha;
+
+/**
+ *
+ * @author Diego
+ */
+public class Velha {
+
+    private String[][] velha = new String[3][3];
+    private String ultimoJogador;
+
+    public void Velha(int x, int y, String jogador) {
+        this.getVelha()[x][y] = jogador;
+        if (jogador.equals("X")) {
+            this.setUltimoJogador("O");
+        } else {
+            this.setUltimoJogador("X");
+        }
+    }
+
+    public void JogadasDisponiveis() {
+        System.out.println("Jogadas Disponíveis para " + this.ultimoJogador);
+        buscarJogadas(this.getVelha(), 0, 0);
+        System.out.println("Fim!");
+    }
+
+    public static void buscarJogadas(String[][] velha, int v, int h) {
+        if (velha[v][h] == null) {
+            System.out.println("velha[" + v + "][" + h + "]");
+            if (h < 3 - 1) {
+                buscarJogadas(velha, v, h + 1);
+            } else {
+                h = 0;
+                if (v < 3 - 1) {
+                    buscarJogadas(velha, v + 1, h);
+                }
+            }
+        } else {
+            if (h < 3 - 1) {
+                buscarJogadas(velha, v, h + 1);
+            } else {
+                h = 0;
+                if (v < 3 - 1) {
+                    buscarJogadas(velha, v + 1, h);
+                } else {
+                    System.out.println("Fim!");
+                }
+            }
+        }
+    }
+
+    public String[][] getVelha() {
+        return velha;
+    }
+
+    public void setVelha(String[][] velha) {
+        this.velha = velha;
+    }
+
+    public String getUltimoJogador() {
+        return ultimoJogador;
+    }
+
+    public void setUltimoJogador(String ultimoJogador) {
+        this.ultimoJogador = ultimoJogador;
+    }
+}
